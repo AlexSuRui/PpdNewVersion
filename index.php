7cead68dad5c4f2814dfe0b9a3b64d726f92650d
AlexSuRui <surui.alex@gmail.com>
2016-03-22 11:25:03 +0100

    22

--- index.php
+++ index.php
@@ -8,49 +8,71 @@
 		if (isset($_SESSION["utilisateur"]))
 			$me = $_SESSION["utilisateur"];
 	?>
-    
 <!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Accueil</title>
-    <!-- Tell the browser to be responsive to screen width -->
-    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <!-- Bootstrap 3.3.5 -->
-    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
-    <!-- Ionicons -->
-    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-    <!-- Animate.css -->
-    <link rel="stylesheet" href="css/animate.css">
-    <!-- Theme style -->
-    <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
-    <!-- AdminLTE Skins. Choose a skin from the css/skins
-         folder instead of downloading all of them to reduce the load. -->
-    <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
-	  <!-- CSS Sytle -->
-    <link rel="stylesheet" href="css/cards.css">
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Voltage &mdash; 100% Free Fully Responsive HTML5 Template by FREEHTML5.co</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FREEHTML5.CO" />
 
-  </head>
-  <!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
-  <body id="header">
+  	<!-- 
+	//////////////////////////////////////////////////////
 
-     <div class="">
-          <nav class="fh5co-nav-style-1 " role="navigation" data-offcanvass-position="fh5co-offcanvass-left">
-              <div class="container">
-                <div class="col-md-3 fh5co-logo">
-                  <a href="#" class="js-fh5co-mobile-toggle fh5co-nav-toggle"><i></i></a>
-                  <a href="#">IDV Paris Descartes</a>
-                </div>
-                <div class="col-md-3 col-md-offset-4 text-center fh5co-link-wrap">
+	FREE HTML5 TEMPLATE 
+	DESIGNED & DEVELOPED by FREEHTML5.CO
+		
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+	
+	<!-- Google Fonts -->
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,700|Monsterrat:400,700|Merriweather:400,300italic,700' rel='stylesheet' type='text/css'>
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="css/icomoon.css">
+	<!-- Magnific Popup-->
+	<link rel="stylesheet" href="css/magnific-popup.css">
+	<!-- Owl Carousel -->
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="css/bootstrap.css">
+	
+	<!-- Cards -->
+	<link rel="stylesheet" href="css/cards.css">
+
+	<!-- Modernizr JS -->
+	<script src="js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+
+	</head>
+	<body>
+	
+	<div >
+		<nav class="fh5co-nav-style-1" role="navigation" data-offcanvass-position="fh5co-offcanvass-left">
+			<div class="container">
+				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 fh5co-logo">
+					 <a href="#" class="js-fh5co-mobile-toggle fh5co-nav-toggle"><i></i></a>
+	                 <a href="#">IDV Paris Descartes</a>
+				</div>
+				 <div class="col-md-3 col-md-offset-4 text-center fh5co-link-wrap">
                   <ul data-offcanvass="yes">
                      <?php 
                               if(isset($me)) {?>
@@ -64,7 +86,7 @@
                               <?php } ?>
                   </ul>
                 </div> 
-                <div class="col-md-2 text-right fh5co-link-wrap">
+				<div class="col-md-2 text-right fh5co-link-wrap">
                   <ul class="fh5co-special" data-offcanvass="yes">
                      <?php 
                               if(isset($me)) {?>
@@ -81,62 +103,60 @@
                               <?php } ?>
                   </ul>
                 </div>
-              </div>
-          </nav>
-          
-          <div class="fh5co-cover fh5co-cover-style-2 js-full-height" data-stellar-background-ratio="0.5" data-next="yes"  style="background-image: url(images/full_1.jpg);">
+			</div>
+		</nav>
 
-              <div class="fh5co-overlay">
-              </div>
-              <div class="fh5co-cover-text">
+
+		<div class="fh5co-cover fh5co-cover-style-2 js-full-height" data-stellar-background-ratio="0.5" data-next="yes"  style="background-image: url(images/full_1.jpg);">
+		  	<span class="scroll-btn wow fadeInUp" data-wow-duration="1s" data-wow-delay="1.4s">
+				<a href="#">
+					<span class="mouse"><span></span></span>
+				</a>
+			</span>
+			<div class="fh5co-overlay"></div>
+			<div class="fh5co-cover-text">
                 <div class="container">
                   <div class="row" >
-
                     <div class="col-md-10 col-md-offset-1 full-height js-full-height col-xs-10">
-
                       <div class="fh5co-cover-intro">
-
                         <h3 class="cover-text-lead wow fadeInUp" data-wow-duration="1s" data-wow-delay=".5s">Enrichissement sémantique des images biomédicales</h3>
                         <div class="fh5co-post wow fadeInLeft"  data-wow-duration="1s" data-wow-delay="1.1s">
                           <img src ="images/bg.jpg" height="250px" width="750px">
-                        </div>
-                        
+                        </div>                        
                       </div>
-
                     </div>
                   </div>
                 </div>  
               </div>
+		</div>
 
-          </div>
-     </div>
-  </body>
+		
+	</div>
+	<!-- END page-->
 
-  <script src="js/jquery.min.js"></script>
-  <!-- jQuery Easing -->
-  <script src="js/jquery.easing.1.3.js"></script>
-  <!-- Bootstrap -->
-  <script src="js/bootstrap.min.js"></script>
-  <!-- Waypoints -->
+	<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
    <script src="js/jquery.waypoints.min.js"></script>
-  <!-- Owl Carousel -->
-  <script src="js/owl.carousel.min.js"></script>
-  <!-- Stellar -->
-  <script src="js/jquery.stellar.min.js"></script>
-  <!-- countTo -->
-  <script src="js/jquery.countTo.js"></script>
-  <!-- WOW -->
-  <script src="js/wow.min.js"></script>
-  <script>
-    new WOW().init();
-  </script>
+	<!-- Owl Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<!-- Stellar -->
+	<script src="js/jquery.stellar.min.js"></script>
+	<!-- countTo -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- WOW -->
+	<script src="js/wow.min.js"></script>
 	<script>
-	$(document).ready(function() {
-    $('#Carousel').carousel({
-        interval: 3000
-    })
-});
+		new WOW().init();
 	</script>
-  </body>
-  
+	<!-- Main -->
+	<script src="js/main.js"></script>
+
+	</body>
 </html>
